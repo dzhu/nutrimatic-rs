c9ab7d5f30798fe1a26be65fcefd0993b3eb3cd6
Danny Zhu <dzhu@dzhu.us>
2021-02-10 19:30:25 -0800

    Add node reader based on function pointers
    The last method of reading a node, though it had big wins over the one
    before that in terms of laziness, suffered from having to do a bunch of
    branching on sizes despite each instance of LinkReader only ever having
    to deal with one particular node layout. This new technique dispatches
    to function pointers specialized for each layout, ultimately becoming
    about 50% faster yet.

--- src/lib.rs
+++ src/lib.rs
@@ -224,6 +224,99 @@ pub fn read_node2(buf: &[u8], ind: usize, freq: usize) -> LinkReader {
     LinkReader::new(buf, ind, freq)
 }
 
+fn read_node_00(buf: &[u8], base: usize, _ind: usize, freq: usize) -> Link {
+    Link {
+        ch: buf[base],
+        freq: freq,
+        loc: Some(base),
+    }
+}
+
+fn read_node_10(buf: &[u8], base: usize, ind: usize, _freq: usize) -> Link {
+    Link {
+        ch: buf[base + 2 * ind],
+        freq: buf[base + 2 * ind + 1] as usize,
+        loc: None,
+    }
+}
+
+fn read_node_11(buf: &[u8], base: usize, ind: usize, _freq: usize) -> Link {
+    let ofs = buf[base + 3 * ind + 2];
+    Link {
+        ch: buf[base + 3 * ind],
+        freq: buf[base + 3 * ind + 1] as usize,
+        loc: if ofs == u8::MAX {
+            None
+        } else {
+            Some(base - ofs as usize)
+        },
+    }
+}
+
+fn read_node_12(buf: &[u8], base: usize, ind: usize, _freq: usize) -> Link {
+    let ofs = LE::read_u16(&buf[base + 4 * ind + 2..]);
+    Link {
+        ch: buf[base + 4 * ind],
+        freq: buf[base + 4 * ind + 1] as usize,
+        loc: if ofs == u16::MAX {
+            None
+        } else {
+            Some(base - ofs as usize)
+        },
+    }
+}
+
+fn read_node_22(buf: &[u8], base: usize, ind: usize, _freq: usize) -> Link {
+    let ofs = LE::read_u16(&buf[base + 5 * ind + 3..]);
+    Link {
+        ch: buf[base + 5 * ind],
+        freq: LE::read_u16(&buf[base + 5 * ind + 1..]) as usize,
+        loc: if ofs == u16::MAX {
+            None
+        } else {
+            Some(base - ofs as usize)
+        },
+    }
+}
+
+fn read_node_88(buf: &[u8], base: usize, ind: usize, _freq: usize) -> Link {
+    let ofs = LE::read_u64(&buf[base + 17 * ind + 9..]);
+    Link {
+        ch: buf[base + 17 * ind],
+        freq: LE::read_u64(&buf[base + 17 * ind + 1..]) as usize,
+        loc: if ofs == u64::MAX {
+            None
+        } else {
+            Some(base - ofs as usize)
+        },
+    }
+}
+
+pub fn read_node3(
+    buf: &[u8],
+    ind: usize,
+) -> (usize, usize, fn(&[u8], usize, usize, usize) -> Link) {
+    let ind = ind - 1;
+    let sig = buf[ind];
+    let (sig_base, elem_bytes, func): (_, _, fn(&[u8], usize, usize, usize) -> Link) = match sig {
+        0x20..=0x7f => return (1, ind, read_node_00),
+        0x00..=0x1f => (0x00, 2, read_node_10),
+        0x80..=0x9f => (0x80, 3, read_node_11),
+        0xa0..=0xbf => (0xa0, 4, read_node_12),
+        0xc0..=0xdf => (0xc0, 5, read_node_22),
+        0xe0..=0xff => (0xe0, 17, read_node_88),
+    };
+
+    let (ind, num) = match buf[ind] - sig_base {
+        0 => (ind - 1, buf[ind - 1]),
+        d if d < 0x20 => (ind, d),
+        _ => unreachable!(),
+    };
+
+    let base = ind - elem_bytes * num as usize;
+    (num.into(), base, func)
+}
+
 pub fn find_link(buf: &[u8], ind: usize, freq: usize, ch: u8) -> Option<Link> {
     let mut links = read_node(buf, ind, freq);
     match links.binary_search_by_key(&ch, |link| link.ch) {
