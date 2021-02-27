f9fdf89a074e321a1c2a5ba81ac0850d468ea103
Danny Zhu <dzhu@dzhu.us>
2021-02-27 13:03:34 -0800

    Remove unnecessary lifetime annotations
    Pointed out by Clippy.

--- src/node_types.rs
+++ src/node_types.rs
@@ -1,11 +1,6 @@
 use byteorder::{ByteOrder, LE};
 
-pub(crate) fn read_00<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    _ind: usize,
-    freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_00(buf: &[u8], base: usize, _ind: usize, freq: u64) -> crate::Node {
     crate::Node {
         freq,
         ch: buf[base],
@@ -14,12 +9,7 @@ pub(crate) fn read_00<'buf>(
     }
 }
 
-pub(crate) fn read_10<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    ind: usize,
-    _freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_10(buf: &[u8], base: usize, ind: usize, _freq: u64) -> crate::Node {
     crate::Node {
         freq: buf[base + 2 * ind + 1] as u64,
         ch: buf[base + 2 * ind],
@@ -28,12 +18,7 @@ pub(crate) fn read_10<'buf>(
     }
 }
 
-pub(crate) fn read_11<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    ind: usize,
-    _freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_11(buf: &[u8], base: usize, ind: usize, _freq: u64) -> crate::Node {
     let ofs = buf[base + 3 * ind + 2];
     crate::Node {
         freq: buf[base + 3 * ind + 1] as u64,
@@ -47,12 +32,7 @@ pub(crate) fn read_11<'buf>(
     }
 }
 
-pub(crate) fn read_12<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    ind: usize,
-    _freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_12(buf: &[u8], base: usize, ind: usize, _freq: u64) -> crate::Node {
     let ofs = LE::read_u16(&buf[base + 4 * ind + 2..]);
     crate::Node {
         freq: buf[base + 4 * ind + 1] as u64,
@@ -66,12 +46,7 @@ pub(crate) fn read_12<'buf>(
     }
 }
 
-pub(crate) fn read_22<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    ind: usize,
-    _freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_22(buf: &[u8], base: usize, ind: usize, _freq: u64) -> crate::Node {
     let ofs = LE::read_u16(&buf[base + 5 * ind + 3..]);
     crate::Node {
         freq: LE::read_u16(&buf[base + 5 * ind + 1..]) as u64,
@@ -85,12 +60,7 @@ pub(crate) fn read_22<'buf>(
     }
 }
 
-pub(crate) fn read_88<'buf>(
-    buf: &'buf [u8],
-    base: usize,
-    ind: usize,
-    _freq: u64,
-) -> crate::Node<'buf> {
+pub(crate) fn read_88(buf: &[u8], base: usize, ind: usize, _freq: u64) -> crate::Node {
     let ofs = LE::read_u64(&buf[base + 17 * ind + 9..]);
     crate::Node {
         freq: LE::read_u64(&buf[base + 17 * ind + 1..]) as u64,
